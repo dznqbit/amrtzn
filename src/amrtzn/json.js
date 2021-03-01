af9316a71bbd16368657e03749de42f14019f240
Quinton Harris <quinton.harris@carbonfive.com>
2021-03-01 12:18:01 -0800

    Dont explode on JSON deserialization.

--- src/amrtzn/json.js
+++ src/amrtzn/json.js
@@ -3,24 +3,28 @@ function encodeLoanJson(loan) {
 }
 
 function decodeLoanJson(json) {
-  if (!json) {
-    return null;
+  try {
+    if (!json) {
+      return null;
+    }
+
+    let loanDetails = JSON.parse(json);
+
+    loanDetails.loanAmount = Number(loanDetails.loanAmount);
+    loanDetails.loanStart = new Date(loanDetails.loanStart);
+    loanDetails.loanInterest = Number(loanDetails.loanInterest);
+    loanDetails.minimumPayment = Number(loanDetails.minimumPayment);
+    loanDetails.paymentAmount = Number(loanDetails.paymentAmount);
+
+    loanDetails.payments = loanDetails.payments.map(function(mp) {
+      mp.date = new Date(mp.date);
+      return mp;
+    });
+
+    return loanDetails;
+  } catch (exception) {
+    console.error("Could not deserialize loan JSON", exception.message);
   }
-
-  let loanDetails = JSON.parse(json);
-
-  loanDetails.loanAmount = Number(loanDetails.loanAmount);
-  loanDetails.loanStart = new Date(loanDetails.loanStart);
-  loanDetails.loanInterest = Number(loanDetails.loanInterest);
-  loanDetails.minimumPayment = Number(loanDetails.minimumPayment);
-  loanDetails.paymentAmount = Number(loanDetails.paymentAmount);
-
-  loanDetails.payments = loanDetails.payments.map(function(mp) {
-    mp.date = new Date(mp.date);
-    return mp;
-  });
-
-  return loanDetails;
 }
 
 export { encodeLoanJson, decodeLoanJson };
